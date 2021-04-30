5ad1e15beead7cbb3b05ee89669d67efe2dd984d
260007 <81068076+260007@users.noreply.github.com>
2021-04-30 22:18:05 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -1,6 +1,6 @@
 /**
  * @file Activity3.c
- * @author Rahul Jha (rmjha99@gmail.com)
+ * @author Ganesh Shenoy (ganeshshenoy999@gmail.com)
  * @brief // To generate PWM pulses with different duty cycle according to the ADC range 
  * @version 0.1
  * @date 2021-04-30
@@ -20,39 +20,39 @@ char Activity3()
     TCCR1A|=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
     TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);//64 prescalar
     DDRB|=(1<<PB1); //setting portB
-    char temperature=0;
+    char t=0;
     Activity2();
     
     if((ADC>=0) &&(ADC<=209))
     {
         OCR1A=205;      //for 20% PWM
-        temperature=20;      
+        t=20;      
         _delay_ms(20);
     }
 
 
-        if ((ADC>=210)&& (ADC<=509))
+        else if ((ADC>=210)&& (ADC<=509))
         {
         OCR1A=410;    //for 40% PWM
-        temperature=25; 
+        t=25; 
         _delay_ms(200);
         }
 
-        if((ADC>=510)&&(ADC<=709))
+        else if((ADC>=510)&&(ADC<=709))
         {OCR1A=717;   //for 70% PWM
-        temperature=29;
+        t=29;
         _delay_ms(200);
         }
 
-        if((ADC>=710)&&(ADC<=1024))
+        else if((ADC>=710)&&(ADC<=1024))
         {
         OCR1A=974;   //for 95% PWM
-        temperature=33;
+        t=33;
         _delay_ms(200);
 
 
         }
 
-return temperature;  
+return t;  
 
 }
