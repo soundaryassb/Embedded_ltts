6e8a6667d83ec037463e24faf46248e2b2a820d9
260007 <81068076+260007@users.noreply.github.com>
2021-04-30 22:28:59 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,3 +1,13 @@
+/**
+ * @file Activity3.h
+ * @author Ganesh Shenoy (ganeshshenoy999@gmail.com)
+ * @brief Header file for defining activity3
+ * @version 0.1
+ * @date 2021-04-30
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
 /**
