702cffc0678f221b8b65b3ceb7648c10fd6d5bbf
260007 <81068076+260007@users.noreply.github.com>
2021-04-30 23:27:55 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -12,13 +12,13 @@ void USARTInit(uint16_t ubrr_value)
     UBRR0H=(ubrr_value>>8)& 0x00ff;
     //8 bit size
     UCSR0C=(1<<UCSZ00)|(1<<UCSZ01);
-    //Enable receiver and transmitter
+    //Enable transmitter and reciever
     UCSR0B=(1<<RXCIE0)|(1<<TXCIE0)|(1<<TXEN0)|(1<<RXEN0);
 
 }
 char USARTReadChar()
 {
-    while(!(UCSR0A &(1<<RXC0))) //wait till data is received
+    while(!(UCSR0A &(1<<RXC0))) //waiting till data is received
     {
         //do nothing
     }
@@ -27,7 +27,7 @@ char USARTReadChar()
 void USARTWriteChar(char data)
 
 {
-    while (!(UCSR0A & (1<<UDRE0))) //wait till data transmitted
+    while (!(UCSR0A & (1<<UDRE0))) //waiting till data transmitted
     {
         //do nothing
     }
@@ -38,7 +38,7 @@ void USARTWriteChar(char data)
 void Activity4()
 {
 
-     USARTInit(103); //UBRR Value =(F_CPU/(16* 9600))-1
+     USARTInit(103); //UBRR Value =(F_CPU/(16* 9600))-1(formula from lecture)
 
      char temp=0;
      temp=Activity3();
@@ -47,22 +47,22 @@ void Activity4()
 
         if(temp==20)
         {
-            USARTWriteChar(temp); // temperature=20 transmitted by micro to serail monitor
+            USARTWriteChar(temp); // temperature=20 transmitted by microcontroller to serial monitor
             _delay_ms(200);
         }
          if(temp==25)
         {
-            USARTWriteChar(temp); // temperature=20 transmitted by micro to serial monitor
+            USARTWriteChar(temp); // temperature=25 transmitted by microcontroller to serial monitor
             _delay_ms(200);
         }
          if(temp==29)
         {
-            USARTWriteChar(temp);//temperature=20 transmitted by micro to serial monitor
+            USARTWriteChar(temp);//temperature=29 transmitted by microcontroller to serial monitor
             _delay_ms(200);
         }
          if(temp==33)
         {
-            USARTWriteChar(temp);//temperature=20 transmitted by micro to serial monitor
+            USARTWriteChar(temp);//temperature=33 transmitted by microcontroller to serial monitor
             _delay_ms(200);
         }
 
