00f5e7a7e8eb6040fb78815d049cb6deb0589536
soundaryasb <68370011+soundaryassb@users.noreply.github.com>
2021-07-24 21:18:25 +0530

    Update 3_source_Activity1.c

--- 3_source_Activity1.c
+++ 3_source_Activity1.c
@@ -1,13 +1,3 @@
-/**
- * @file Activity1.c
- * @author Ganesh Shenoy (ganeshshenoy999@gmail.com)
- * @brief // configuration of pins
- * @version 0.1
- * @date 2021-04-29
- *
- * @copyright Copyright (c) 2021
- *
- */
 #include <avr/io.h>
 #include <util/delay.h>
 #include "Activity3.h"
